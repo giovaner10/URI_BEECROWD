2ebc27032aa1c437dcbdff9047252b6067aa6c98
unknown <giovanerr10@gmail.com>
2022-03-30 15:08:37 -0300

    1012

---
+++ 1012_Area.py
@@ -0,0 +1,12 @@
+P=input().split()
+A,B,C=P
+a1=float(A)*float(C)/2
+b1=(float(C)**2)*3.14159
+c1=(float(A)+float(B))*float(C)/2
+d1=float(B)**2
+e1=float(A)*float(B)
+print('TRIANGULO: %0.3f' %a1)
+print('CIRCULO: %0.3f' %b1)
+print('TRAPEZIO: %0.3f' %c1)
+print('QUADRADO: %0.3f' %d1)
+print('RETANGULO: %0.3f' %e1)
