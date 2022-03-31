cea39675d989b12ef3fb25caec948a51aacfa731
giovaner10 <giovanerr10@gmail.com>
2022-03-31 17:24:13 -0300

    1050

---
+++ 1050_DDD.py
@@ -0,0 +1,20 @@
+d=int(input())
+if d==61:
+    print('Brasilia')
+elif d==71:
+    print('Salvador')
+elif d==11:
+    print('Sao Paulo')
+elif d==21:
+    print('Rio de Janeiro')
+elif d==32:
+    print('Juiz de Fora')
+elif d==19:
+    print('Campinas')
+elif d==27:
+    print('Vitoria')
+elif d==31:
+    print('Belo Horizonte')
+else:
+    print('DDD nao cadastrado')
+    
