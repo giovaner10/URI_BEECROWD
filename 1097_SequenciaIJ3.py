85d24d0d21b4732b3e3685f47cc7572c34a076f1
giovaner10 <giovanerr10@gmail.com>
2022-03-31 20:16:32 -0300

    1997

---
+++ 1097_SequenciaIJ3.py
@@ -0,0 +1,15 @@
+i = 1
+j=7
+cont=0
+while cont < 15:
+
+    print('I={} J={}' .format(i,j))
+    j-=1
+    print('I={} J={}' .format(i,j))
+    j-=1
+    print('I={} J={}' .format(i,j))
+
+    i+=2
+    j+=4
+
+    cont+=3
