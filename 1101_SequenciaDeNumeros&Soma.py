d8ee50dfffdb0bb1fa4cf1c042fa6ec550e407ef
giovaner10 <giovanerr10@gmail.com>
2022-03-31 20:23:42 -0300

    1101

---
+++ 1101_SequenciaDeNumeros&Soma.py
@@ -0,0 +1,27 @@
+aux = 0
+maior = 0
+menor = 0
+
+
+cond = True
+while cond:
+        valor1, valor2 = map(int, input().split())
+   
+        if(valor1 <= 0 or valor2 <= 0):
+                break
+
+        maior = valor1 if valor1 > valor2 else valor2
+        menor = valor2 if valor2 < valor1 else valor1
+   
+        if maior > menor :
+                aux = maior
+                maior = menor
+                menor = aux
+               
+        soma = 0
+       
+        while maior <= menor :
+         print(maior, end=' ')
+         soma += maior
+         maior+=1
+        print("Sum=%d"%soma)
