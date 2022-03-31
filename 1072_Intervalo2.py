57fcc89507c6b67c00abd0cd5450ea0308897160
giovaner10 <giovanerr10@gmail.com>
2022-03-31 19:46:59 -0300

    1072

---
+++ 1072_Intervalo2.py
@@ -0,0 +1,13 @@
+n=int(input())
+cont=0
+intt = out = 0
+while cont < n:
+    v=int(input())
+    if (10)<=v<=(20):
+        intt +=1
+    else:
+        out+=1
+    cont  +=1
+    
+print('{} in' .format(intt))
+print('{} out' .format(out))
